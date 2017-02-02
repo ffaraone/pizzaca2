d9b20bab586b6b05e693bc9aa95c96f078f2ef09
Francesco Faraone <ffaraone@gmail.com>
2017-02-02 17:14:31 +0100

    fix version
    gitignore private

--- pizzaca2/__init__.py
+++ pizzaca2/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.1.0'
+VERSION = (1, 0, 2)
+
+__version__ = '.'.join([str(n) for n in VERSION])
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
+
+
+def get_version():
+    return __version__
